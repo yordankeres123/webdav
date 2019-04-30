ec942535b9dd2357aa8a7d02ea29bf8588b0dab6
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-05-01 00:22:36 +0700

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,102 @@
+#!/system/xbin/bash
+#mau ngapain lo cok ? ngerecode
+#yaudah lah cok recode aja tapi jangan
+#diganti author cok
+clear
+figlet GREEN | lolcat
+figlet DRAGON | lolcat
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" | lolcat
+echo
+echo " Ketik 1 untuk  webdav" | lolcat
+echo " author Bigbos Starla Biasa" | lolcat
+echo " dipanggil GREEN DRAGON " | lolcat
+echo " plis subscribe chanel saya" | lolcat
+echo " nama chanel : Bigbos Starla" | lolcat
+echo " Fb : Yordan penceng" | lolcat
+echo " Fb ke 2 : Bigbos starla" | lolcat
+echo
+echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" | lolcat
+echo
+echo "###################################" | lolcat
+echo
+echo " 1.DEFACE WEBDAV METODE " | lolcat
+echo
+echo " 2.KLIK NO 1 UNTUK DOWNLOAD FILENYA" | lolcat
+echo
+echo " 0.KELUAR DARI TOOLS" | lolcat
+echo
+echo "###################################"
+read -p "GREENDRAGON# " cok
+if [ $cok = 1 ] || [ $cok = 01 ]
+then
+clear
+toilet GREEN | lolcat
+toilet DRAGON | lolcat
+echo
+echo "Copy Link Dibawah Ini terus pastekan"
+echo "Digoogle untuk mendownload filenya"
+echo "linknya dibawah ini "
+echo
+echo "https://shortid.co/t7rGx"
+echo
+echo "silahkan copy link diatas"
+echo "Jika Sudah Klik nomor 2 Untuk Lanjut" | lolcat
+sleep 10
+cd webdav
+sh webdav.sh
+fi
+
+if [ $cok = 2 ] || [ $cok = 02 ]
+then
+clear
+echo "GREEN DRAGON | BIGBOS STATLA " | lolcat
+echo "OTW MASUK KE WEBDAVNYA YAH" | lolcat
+echo
+cd /sdcard
+php access.php
+fi
+
+if [ $cok = 0 ] || [ $cok = 00 ]
+then
+clear
+echo "Makasih telah menggunakan tools ini" | lolcat
+echo "THX TO CYB3R POP1CE +62 " | lolcat
+echo "Gue tinggal yah mas wasalam" | lolcat
+echo "LOADING"
+echo "■■■■"
+echo "■■■■■" | lolcat
+echo "■■■■■■" | lolcat
+echo "■■■■■■■" | lolcat
+echo "■■■■■■■■" | lolcat 
+echo "■■■■■■■■■" | lolcat
+echo "■■■■■■■■■■" | lolcat
+echo "■■■■■■■■■■■" | lolcat
+clear
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+echo "■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■"
+sleep 2
+exit
+fi
+
